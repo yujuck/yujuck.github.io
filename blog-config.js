2d9e343f906628bd5bd5cd7eddb889ddc9eda7d6
yujuck <yujin3278@gmail.com>
2023-06-06 23:57:49 +0900

    Feat: 블로그 테마 적용, 첫 게시글

---
+++ blog-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: "yujuck",
+  description: "찾아가는 과정의 기록",
+  author: "yujuck",
+  siteUrl: "https://yujuck.github.io",
+  links: {
+    github: "https://github.com/yujuck",
+    email: "mailto:yujin3278@gmail.com",
+  },
+  utterances: {
+    repo: "yujuck/yujuck.github.io",
+    type: "pathname",
+  },
+}
