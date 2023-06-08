0fdc8ff196db89faa9a38d3aaaeceb2a22ba4bbe
yujuck <yujin3278@gmail.com>
2023-06-08 22:22:26 +0900

    google search console meta 태그 추가

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -9,6 +9,7 @@ const SEO = ({ title, description, url }) => {
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
       <meta property="og:image" content={`${siteUrl}/og-image.png`} />
+      <meta name="google-site-verification" content="t63tcQHKII8QHvD30qV7iw0CRDlaTPTm10vygufRH6c" />
       {description && <meta name="description" content={description} />}
       {description && <meta property="og:description" content={description} />}
     </Helmet>
